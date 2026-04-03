54818a9351646610d6ce7d7e52663cc0f88714c2
Jenea-G <eugeniagurulea@gmail.com>
2026-04-03 13:03:46 -0400

    add complete task functionality

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,26 @@ import TaskList from "./components/tasklist";
 import Footer from "./components/footer";
 import "./components/styles.css";
 
+type Task = {
+  text: string;
+  checked: boolean;
+};
+
 export default function Home() {
-  const [tasks, setTasks] = useState<string[]>([]);
+  const [tasks, setTasks] = useState<Task[]>([]);
 
   const addTask = (task: string) => {
-    setTasks([...tasks, task]);
-  }
+    setTasks([...tasks, { text: task, checked: false }]);
+  };
+
   const deleteTask = (index: number) => {
-    setTasks(tasks.filter((_, i) => i !== index));
+    setTasks(tasks.filter((task, i) => i !== index));
+  };
+
+  const toggleTask = (index: number) => {
+    const updated = [...tasks];
+    updated[index].checked = !updated[index].checked;
+    setTasks(updated);
   };
 
   return (
@@ -20,12 +32,14 @@ export default function Home() {
       <main className="container">
         <h1>To-Do App</h1>
         <TaskInput addTask={addTask} />
-        <TaskList tasks={tasks} deleteTask={deleteTask} />
-      
+        <TaskList
+          tasks={tasks}
+          deleteTask={deleteTask}
+          toggleTask={toggleTask}
+        />
       </main>
 
       <Footer />
     </body>
-
   );
 }
